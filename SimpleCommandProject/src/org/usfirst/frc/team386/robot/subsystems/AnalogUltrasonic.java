e17b67fae7fca55a2a70f0938552126b6379c23d
Debbie386 <debbernier@gmail.com>
2017-10-25 17:37:03 -0400

    Basic framework for a command based program. Initial version containing only the tank drive mapped for the Steamworks robot

---
+++ SimpleCommandProject/src/org/usfirst/frc/team386/robot/subsystems/AnalogUltrasonic.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team386.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+/**
+ *
+ */
+public class AnalogUltrasonic extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
