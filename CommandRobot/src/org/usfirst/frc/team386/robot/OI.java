b540bc4cabde874dfd994f2c490da02d869850fc
Anthony Eden <me@anthonyeden.com>
2017-10-24 20:15:39 -0400

    Remove unused imports

--- CommandRobot/src/org/usfirst/frc/team386/robot/OI.java
+++ CommandRobot/src/org/usfirst/frc/team386/robot/OI.java
@@ -1,9 +1,5 @@
 package org.usfirst.frc.team386.robot;
 
-import edu.wpi.first.wpilibj.buttons.Button;
-
-import org.usfirst.frc.team386.robot.commands.ExampleCommand;
-
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
