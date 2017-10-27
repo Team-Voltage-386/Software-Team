f945029e30aaa2adfbdad29ad15d2287760237d7
RadioLogic <nrc1mr.e3@gmail.com>
2017-10-27 10:07:39 -0400

    modified:   SimpleCommandProject/src/org/usfirst/frc/team386/robot/Robot.java

--- SimpleCommandProject/src/org/usfirst/frc/team386/robot/Robot.java
+++ SimpleCommandProject/src/org/usfirst/frc/team386/robot/Robot.java
@@ -91,10 +91,10 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
+		/* This makes sure that the autonomous stops running when
+		 teleop starts running. If you want the autonomous to
+		 continue until interrupted by another command, remove
+		 this line or comment it out. */
 		if (autonomousCommand != null)
 			autonomousCommand.cancel();
 		System.out.println("inside teleopInit");
