7d644d08ebbc9d8d7a729db72ac09e5ec3b7dadb
Ryan Feldbush <ryanfeldbush@gmail.com>
2018-01-23 11:53:28 -0500

    Changed 2SpikeDrive to 386

--- 2SpikeDrive/src/org/usfirst/frc/team386/robot/Robot.java
+++ 2SpikeDrive/src/org/usfirst/frc/team386/robot/Robot.java
@@ -1,11 +1,4 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
-package org.usfirst.frc.team863.robot;
+package org.usfirst.frc.team386.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
