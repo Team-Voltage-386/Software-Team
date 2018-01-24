ff49e1f93acc6971941279ee32818b238c55e2fa
JohhnSmithh <33043348+JohhnSmithh@users.noreply.github.com>
2018-01-23 20:49:22 -0500

    Added Ultrasonic sensor

--- 2SpikeDrive/src/org/usfirst/frc/team386/robot/Robot.java
+++ 2SpikeDrive/src/org/usfirst/frc/team386/robot/Robot.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team386.robot;
 
+import Utility.AnalogUltrasonic;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
@@ -16,33 +17,56 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends IterativeRobot {
 	Talon left = new Talon(9);
 	Talon right = new Talon(8);
+	AnalogUltrasonic ultra1 = new AnalogUltrasonic(0, 1.18, 10.3);
+	AnalogUltrasonic ultra2 = new AnalogUltrasonic(1,1.18,10.3);
 	Joystick controler = new Joystick(0);
 	final String leftName = "Left speed";
 	final String rightName = "Right Speed";
 	boolean on;
+
 	@Override
-	public void robotInit(){
+	public void robotInit() {
 		SmartDashboard.putNumber(leftName, 0);
 		SmartDashboard.putNumber(rightName, 0);
+		SmartDashboard.putNumber("ultra1", ultra1.getInches());
 	}
+
 	@Override
 	public void teleopInit() {
 		on = false;
 	}
+	double difference = Math.abs(ultra2.getInches()-ultra1.getInches());
 	@Override
 	public void teleopPeriodic() {
-		if(controler.getRawButtonPressed(0)) {
+		SmartDashboard.putNumber("ultra2", ultra2.getInches());
+		if (controler.getRawButtonPressed(2)) {
 			SmartDashboard.putNumber(leftName, -1 * SmartDashboard.getNumber(leftName, 0));
 			SmartDashboard.putNumber(rightName, -1 * SmartDashboard.getNumber(rightName, 0));
 		}
-		if(controler.getRawButtonPressed(1)) {
+		if (controler.getRawButtonPressed(1)) {
 			on = !on;
 		}
 		double leftOut = SmartDashboard.getNumber(leftName, 0);
 		double rightOut = SmartDashboard.getNumber(rightName, 0);
 		SmartDashboard.putNumber("Left out", leftOut);
 		SmartDashboard.putNumber("Right out", rightOut);
-		left.set(leftOut);
-		right.set(rightOut);
+		if (on) {
+			SmartDashboard.putBoolean("button pressed", on);
+			if (SmartDashboard.getNumber(leftName, 0) > 0) {
+				if (ultra2.getInches() > 2) {
+					left.set(leftOut);
+					right.set(rightOut);
+				} else {
+					left.set(0);
+					right.set(0);
+				}
+			} else {
+				left.set(leftOut);
+				right.set(rightOut);
+			}
+		} else {
+			left.set(0);
+			right.set(0);
+		}
 	}
 }
