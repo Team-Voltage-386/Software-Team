38b078218746ea99652d346fe1b11ca6121c9f42
Anthony Eden <me@anthonyeden.com>
2018-01-27 21:29:29 -0500

    Method and class documentation and code cleanup

--- TyRapXVIII/src/org/usfirst/frc/team386/robot/subsystems/DriveSubsystem.java
+++ TyRapXVIII/src/org/usfirst/frc/team386/robot/subsystems/DriveSubsystem.java
@@ -11,9 +11,16 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
- *
+ * The DriveSubsystem is responsible for all drive logic. It is intended to be
+ * used in both autonomous as well as teleoperated mode.
  */
 public class DriveSubsystem extends Subsystem {
+    public static final DoubleSolenoid.Value LOW_GEAR = DoubleSolenoid.Value.kForward;
+    public static final DoubleSolenoid.Value HIGH_GEAR = DoubleSolenoid.Value.kReverse;
+
+    private static final double ENCODER_RATIO = 3.0;
+    private static final double TICKS_PER_ROTATION = 256;
+
     WPI_TalonSRX frontLeft = new WPI_TalonSRX(1); /* device IDs here (1 of 2) */
     WPI_TalonSRX frontRight = new WPI_TalonSRX(4);
 
@@ -28,11 +35,15 @@ public class DriveSubsystem extends Subsystem {
     Encoder leftEncoder = new Encoder(0, 1);
     Encoder rightEncoder = new Encoder(2, 3);
 
+    /**
+     * Construct a new DriveSubsystem.
+     */
     public DriveSubsystem() {
 	leftSlave1.follow(frontLeft);
 	leftSlave2.follow(frontLeft);
 	rightSlave1.follow(frontRight);
 	rightSlave2.follow(frontRight);
+
 	frontRight.configContinuousCurrentLimit(20, 0);
 	frontLeft.configContinuousCurrentLimit(20, 0);
 	frontRight.enableCurrentLimit(true);
@@ -40,13 +51,30 @@ public class DriveSubsystem extends Subsystem {
 
 	frontRight.configOpenloopRamp(.1, 0);
 	frontLeft.configOpenloopRamp(.11, 0);
+
 	compressor.start();
-	solenoid.set(DoubleSolenoid.Value.kForward);
+
+	solenoid.set(LOW_GEAR);
     }
 
-    public void drive(double xSpeed, double zRotation) {
-	// drive.arcadeDrive(xSpeed, zRotation);
-	drive.arcadeDrive(deadBand(-1 * xSpeed, .1), deadBand(zRotation, .1));
+    /**
+     * Sets a default command that is run at start and whenever no other command is
+     * running.
+     */
+    public void initDefaultCommand() {
+	setDefaultCommand(new ArcadeDrive());
+    }
+
+    /**
+     * Drive using arcade-style values: speed and rotation.
+     * 
+     * @param xSpeed
+     *            The speed, forward (positive values) or reverse (negative values)
+     * @param zRotation
+     *            The rotation
+     */
+    public void drive(double speed, double rotation) {
+	drive.arcadeDrive(deadBand(-1 * speed, .1), deadBand(rotation, .1));
     }
 
     private double deadBand(double in, double limit) {
@@ -57,40 +85,43 @@ public class DriveSubsystem extends Subsystem {
 	}
     }
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
-    public void initDefaultCommand() {
-	// Set the default command for a subsystem here.
-	setDefaultCommand(new ArcadeDrive());
-    }
-
+    /**
+     * Shift gears. This method will shift to the opposite of the current gear.
+     */
     public void shift() {
-	// boolean buttonPressed = /* Robot.right.getRawButton(1) */
-	// Robot.manipulator.getRawButton(5);
-
-	if (solenoid.get() == DoubleSolenoid.Value.kReverse) {
-	    solenoid.set(DoubleSolenoid.Value.kForward);
+	if (solenoid.get() == HIGH_GEAR) {
+	    solenoid.set(LOW_GEAR);
 	} else {
-	    solenoid.set(DoubleSolenoid.Value.kReverse);
+	    solenoid.set(HIGH_GEAR);
 	}
-
     }
 
+    /**
+     * Zero all drive encoders.
+     */
     public void resetEncoders() {
 	leftEncoder.reset();
 	rightEncoder.reset();
     }
 
+    /**
+     * Move forward the specific number of inches.
+     * 
+     * @param inches
+     *            Inches to move forward
+     */
     public void moveForward(double inches) {
-	double cir = 18.8;
-	double encoderRatio = 3;
-	double ticksPerRotation = 256;
-	// double ticksRequired = (inches * ticksPerRotation) / (cir * encoderRatio);
-	double ticksRequired = ((ticksPerRotation * encoderRatio) / cir) * inches;
+	double speed = 1.0;
+	double wheelCircumference = 18.8;
+
+	// This is the original algorithm pulled in from the PhoenixTankDrive project
+	// double ticksRequired = (inches * TICKS_PER_ROTATION) / (wheelCircumference *
+	// ENCODER_RATIO);
+
+	double ticksRequired = ((TICKS_PER_ROTATION * ENCODER_RATIO) / wheelCircumference) * inches;
 	while (Math.abs(leftEncoder.getRaw()) < ticksRequired) {
-	    drive.tankDrive(1.0, 1.0);
+	    drive.tankDrive(speed, speed);
 	}
-	drive.tankDrive(0, 0); // drives forward at 0 speed
+	drive.tankDrive(0, 0); // stops driving forward
     }
 }
