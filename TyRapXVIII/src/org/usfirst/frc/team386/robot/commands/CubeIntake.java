02cb2d1e9536e9c29b07eda0f2bc4cf6c497b944
Anthony Eden <me@anthonyeden.com>
2018-02-03 20:32:54 -0500

    Switch no-op cube and elevator commands to instance commands

--- TyRapXVIII/src/org/usfirst/frc/team386/robot/commands/CubeIntake.java
+++ TyRapXVIII/src/org/usfirst/frc/team386/robot/commands/CubeIntake.java
@@ -2,12 +2,12 @@ package org.usfirst.frc.team386.robot.commands;
 
 import org.usfirst.frc.team386.robot.Robot;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 
 /**
  *
  */
-public class CubeIntake extends Command {
+public class CubeIntake extends InstantCommand {
 
     public CubeIntake() {
 	// Use requires() here to declare subsystem dependencies
@@ -16,24 +16,5 @@ public class CubeIntake extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-	return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
     }
 }
