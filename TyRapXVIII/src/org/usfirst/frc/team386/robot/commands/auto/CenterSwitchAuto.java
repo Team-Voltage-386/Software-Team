d3ae1fb7fc00d8a8e08392c09dc35df735f34efa
Anthony Eden <me@anthonyeden.com>
2018-02-03 20:44:40 -0500

    Demonstrate how we can use inner classes to implement the command groups for each strategy

--- TyRapXVIII/src/org/usfirst/frc/team386/robot/commands/auto/CenterSwitchAuto.java
+++ TyRapXVIII/src/org/usfirst/frc/team386/robot/commands/auto/CenterSwitchAuto.java
@@ -2,7 +2,14 @@ package org.usfirst.frc.team386.robot.commands.auto;
 
 import org.usfirst.frc.team386.robot.OI;
 import org.usfirst.frc.team386.robot.Robot;
+import org.usfirst.frc.team386.robot.commands.CubeRelease;
+import org.usfirst.frc.team386.robot.commands.DriveForward;
+import org.usfirst.frc.team386.robot.commands.ElevatorRaise;
+import org.usfirst.frc.team386.robot.commands.LowerIntake;
+import org.usfirst.frc.team386.robot.commands.TurnLeft;
+import org.usfirst.frc.team386.robot.commands.TurnRight;
 
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 
 /**
@@ -24,5 +31,40 @@ public class CenterSwitchAuto extends InstantCommand {
 	} else {
 	    new CenterSwitchAutoRight().start();
 	}
+    }
+
+    /**
+     * Auto mode for left switch starting from center.
+     */
+    class CenterSwitchAutoLeft extends CommandGroup {
+
+	public CenterSwitchAutoLeft() {
+	    addSequential(new LowerIntake());
+	    addSequential(new DriveForward(18));
+	    addSequential(new TurnLeft(45));
+	    addSequential(new DriveForward(75));
+	    addSequential(new TurnRight(45));
+	    addSequential(new DriveForward(25));
+	    addSequential(new ElevatorRaise());
+	    addSequential(new CubeRelease());
+	}
+    }
+
+    /**
+     * Auto mode for right switch starting from center.
+     */
+    class CenterSwitchAutoRight extends CommandGroup {
+
+	public CenterSwitchAutoRight() {
+	    addSequential(new LowerIntake());
+	    addSequential(new DriveForward(18));
+	    addSequential(new TurnRight(45));
+	    addSequential(new DriveForward(75));
+	    addSequential(new TurnLeft(40));
+	    addSequential(new DriveForward(26));
+	    addSequential(new ElevatorRaise());
+	    addSequential(new CubeRelease());
+	}
+
     }
 }
