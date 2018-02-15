3e2f5596c1136230afa6edbfa32c7c7298df34a7
Ryan Feldbush <ryanfeldbush@gmail.com>
2018-02-14 20:57:01 -0500

    Added methods to elevatorSubsystem

--- TyRapXVIII/src/org/usfirst/frc/team386/robot/subsystems/ElevatorSubsystem.java
+++ TyRapXVIII/src/org/usfirst/frc/team386/robot/subsystems/ElevatorSubsystem.java
@@ -2,7 +2,9 @@ package org.usfirst.frc.team386.robot.subsystems;
 
 import org.usfirst.frc.team386.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -13,6 +15,9 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class ElevatorSubsystem extends Subsystem {
     Spark elevatorSpark = new Spark(RobotMap.elevatorSpark);
     Encoder elevatorEncoder = new Encoder(1, 2); // find out actual values
+    Solenoid chainBreaker = new Solenoid(20);
+    DoubleSolenoid leftArm = new DoubleSolenoid(18, 19);
+    DoubleSolenoid rightArm = new DoubleSolenoid(16, 17);
 
     /**
      * Update the smart dashboard with diagnostics values.
@@ -51,4 +56,17 @@ public class ElevatorSubsystem extends Subsystem {
 	}
     }
 
+    public void breakChain() {
+	chainBreaker.set(true);
+    }
+
+    public void shiftArms() {
+	if (leftArm.get() == DoubleSolenoid.Value.kReverse) {
+	    leftArm.set(DoubleSolenoid.Value.kForward);
+	    rightArm.set(DoubleSolenoid.Value.kForward);
+	} else {
+	    leftArm.set(DoubleSolenoid.Value.kReverse);
+	    rightArm.set(DoubleSolenoid.Value.kReverse);
+	}
+    }
 }
