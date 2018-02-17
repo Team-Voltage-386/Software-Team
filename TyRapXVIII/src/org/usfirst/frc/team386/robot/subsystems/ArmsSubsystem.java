0f5fd2a74d97067edf33cf606e0bfabe0dcc366e
Anthony Eden <me@anthonyeden.com>
2018-02-17 11:42:14 -0500

    Clean up the ArmsSubsystem

--- TyRapXVIII/src/org/usfirst/frc/team386/robot/subsystems/ArmsSubsystem.java
+++ TyRapXVIII/src/org/usfirst/frc/team386/robot/subsystems/ArmsSubsystem.java
@@ -6,26 +6,39 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
+ * This subsystem is responsible for lowering and raising the intake arms.
  */
 public class ArmsSubsystem extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+    public static final DoubleSolenoid.Value LOWERED = DoubleSolenoid.Value.kForward;
+    public static final DoubleSolenoid.Value RAISED = DoubleSolenoid.Value.kReverse;
+
     DoubleSolenoid arms = new DoubleSolenoid(RobotMap.armsForwardChannel, RobotMap.armsReverseChannel);
 
+    @Override
     public void initDefaultCommand() {
 	// Set the default command for a subsystem here.
-	// setDefaultCommand(new MySpecialCommand());
     }
 
+    /**
+     * Shift the arms to the other position.
+     */
     public void shiftArms() {
-	if (arms.get() == DoubleSolenoid.Value.kReverse) {
-	    arms.set(DoubleSolenoid.Value.kForward);
-	    arms.set(DoubleSolenoid.Value.kForward);
+	if (arms.get() == RAISED) {
+	    setArms(LOWERED);
 	} else {
-	    arms.set(DoubleSolenoid.Value.kReverse);
-	    arms.set(DoubleSolenoid.Value.kReverse);
+	    setArms(RAISED);
 	}
+    }
+
+    /**
+     * Set arms to specified position.
+     * 
+     * @param position
+     *            The arm position (either LOWERED or RAISED).
+     */
+    public void setArms(DoubleSolenoid.Value position) {
+	arms.set(position);
+	arms.set(position);
     }
 }
