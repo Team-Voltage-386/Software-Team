00994092bd157adbd163ff85d6c3cc410c39a58b
Nick Cottrell <nrc1mr.e3@gmail.com>
2018-02-17 02:56:58 -0500

    Cleaned frcvision

--- FrcVision/src/org/usfirst/frc/team863/robot/Robot.java
+++ FrcVision/src/org/usfirst/frc/team863/robot/Robot.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.opencv.core.Core;
-import org.opencv.core.CvException;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
