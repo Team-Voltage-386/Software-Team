bc737fd7019dc878f66199fc2c2c2ddc2846b157
WillD77 <wadavies92@gmail.com>
2018-05-31 13:59:48 -0400

    Created a small program to swing a golf club
    Iterative based program, uses an encoder as a limit switch.  Simple
    stuff.

---
+++ GolfBotIter/src/org/usfirst/frc/team386/robot/Robot.java
@@ -0,0 +1,90 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team386.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.properties file in the
+ * project.
+ */
+public class Robot extends IterativeRobot {
+	private static final String kDefaultAuto = "Default";
+	private static final String kCustomAuto = "My Auto";
+	private String m_autoSelected;
+	private SendableChooser<String> m_chooser = new SendableChooser<>();
+
+
+    Victor Swing1,Swing2;
+    Joystick Club;
+    Encoder golfEncoder;
+	
+	@Override
+	public void robotInit() {
+		m_chooser.addDefault("Default Auto", kDefaultAuto);
+		m_chooser.addObject("My Auto", kCustomAuto);
+		SmartDashboard.putData("Auto choices", m_chooser);
+		
+		golfEncoder = new Encoder(5, 6, false, Encoder.EncodingType.k4X);
+		golfEncoder.reset();
+		//Swing1 = new Victor (2);
+		Swing2 = new Victor (3);
+		Club = new Joystick(0);
+		//PowerDistributionPanel PDP = new PowerDistributionPanel(0);
+		boolean isInverted  = true;
+		//Swing1.setInverted(isInverted);
+		Swing2.setInverted(isInverted);
+	}
+
+	@Override
+	public void autonomousInit() 
+	{
+	}
+
+	@Override
+	public void autonomousPeriodic() 
+	{
+	}
+
+	@Override
+	public void teleopPeriodic() 
+	{
+		SmartDashboard.putNumber("Golf Encoder", golfEncoder.getRaw());
+		double clubPower = Club.getRawAxis(1);
+		
+		if(Club.getRawButton(1))
+		{
+			golfEncoder.reset();
+		}
+		
+		
+		if(golfEncoder.getDistance() > 20)
+		{
+			clubPower = Math.min(clubPower, 0);
+		}
+		else if(golfEncoder.getDistance() < -20)
+		{
+			clubPower = Math.max(clubPower, 0);
+		}
+		//Swing1.set(Math.pow(clubPower,2));
+		Swing2.set((clubPower/4));
+	}
+
+	@Override
+	public void testPeriodic() {
+	}
+}
